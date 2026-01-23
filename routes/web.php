3d879656092a615a8774b9f0c2046d80b1e81df2
Muzammil <muzammilshahzad894@gmail.com>
2026-01-23 23:45:17 +0500

    add project files and perform login system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminLoginController;
 
-Route::get('/', function () {
-    return view('welcome');
+// check if user is already logged in then redirect to dashboard
+Route::middleware(['guest'])->group(function () {
+    Route::match(['get', 'post'], '/admin/login', [AdminLoginController::class, 'login'])->name('admin.login');
+});
+
+// protected routes
+Route::middleware(['auth'])->group(function () {
+    
 });
